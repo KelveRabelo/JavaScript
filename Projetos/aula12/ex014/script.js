a867c62a49f1f61fd7503860a0fe4cb6542c864f
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-09-30 16:43:58 -0300

    update

--- Projetos/aula12/ex014/script.js
+++ Projetos/aula12/ex014/script.js
@@ -4,25 +4,28 @@ function carregar()
     var msg = window.document.getElementById("msg")
     var img = window.document.getElementById("imagem")
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 19
+    var hora = data.getHours()
+    //var hora = 19
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12)
     {
         //BOM DIA yellow
         img.src = 'imagens/manha.png'
+        document.body.style.background = '#e2cd9f'
         
     }
     else if (hora >= 12 && hora < 18)
     {
         //BOA TARDE blue
         img.src = 'imagens/tarde.jpeg'
+        document.body.style.background = 'orange'
         //foto.style.backgroundColor = "red"
     }
     else
     {
         //BOA NOITE black
         img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#515154'
     }
 }
 
