9d14f5ecda11b87e55b9dd2f2aaa4d9a9d8b1e0e
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-10-03 12:10:22 -0300

    update

---
+++ Projetos/aula13/ambiente.js
@@ -0,0 +1,24 @@
+//ESTRUTURA DE REPETIÇÃO
+
+//while (com teste no inicio)
+var c = 1
+while (c <= 6)
+{
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+//while (com o  teste final)
+do
+{
+    console.log(`Passo ${c}`)
+    c++
+}
+while(c <= 6)
+
+//for (repetição com controle)
+//for(inicio;teste;incremento)
+for(var c=1; c<=10; c++)
+{
+    console.log(`Passo ${c}`)
+}
