382ab77d41313a2b13df100f26d6fe4750a139e0
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-10-05 09:35:56 -0300

    up

--- Projetos/aula15/ambiete.js
+++ Projetos/aula15/ambiete.js
@@ -15,3 +15,28 @@ num.sort()
 
 //MOSTRAR O CONTEÚDO PELA POSIÇÃO(INDICE)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+//MOSTRAR O CONTEÚDO PELA POSIÇÃO(INDICE) USANDO LOOP DE REPETIÇÃO FOR.
+for(var pos = 0; pos < num.length; pos ++)
+{
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+//USADO NAS VERSÕES ATUAIS DO JavaScript Ecma (OBS: FUNCIONA SOMENTE EM ARRAY)
+for(var pos in num)
+{
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+//RETORNA O INDICE DO VALOR INFORMADO DENTRO DO VETOR (OBS: SE RETORNA -1 É PORQUE NÃO ENCONTRO NENHUM VALOR INFORMADO)
+var pos = num.indexOf(7)
+if(pos == -1)
+{
+    console.log('O valor não foi encontrado!')
+}
+else
+{
+    console.log(`O valor esta na posição: ${pos}`)
+}
+
+
