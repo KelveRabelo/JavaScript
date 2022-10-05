b4afdaa7538cc80a2c51da31213d666af1cf6037
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-10-05 15:09:10 -0300

    up

--- Projetos/aula16ex/script.js
+++ Projetos/aula16ex/script.js
@@ -5,7 +5,7 @@ let lista = document.querySelector("select#flista")
 let res = document.querySelector("div#res")
 let valores = []
 
-//VERIFIFICAO
+//VALIDA SE O NUMERO ESTA DENTRO DO RANGE
 function isNumero(n)
 {
     if(Number(n) >= 1 && Number(n) <= 100)
@@ -17,7 +17,7 @@ function isNumero(n)
         return false
     }
 }
-//VERIFICACAO
+//VALIDA SE O NUMERO ESTA NA LISTA
 function inLista(n, l)
 {
     if(l.indexOf(Number(n)) != -1)
@@ -32,17 +32,57 @@ function inLista(n, l)
 
 function adicionar()
 {
-    //VALIDAR SE OS CAMPOS ESTAO VAZIO
-    if(isNumero(num.value) && !inLista(num.value, valores))
+    //VALIDAR AS DUAS VALIDACOES ANTERIORES
+    if(isNumero(num.value) && ! inLista(num.value, valores))
     {
-        alert("Tudo OK!")
+        //ADICIONA O VALOR NO ARRAY (LISTA)
+        valores.push(Number(num.value))
+        //CRIA UM ELEMENTO <OPTION>
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }
     else
     {
         alert("Valor inválido ou já encontrado na lista.")
     }
+    //LIMPA A CAIXA NUM
+    num.value = ''
+    //APONTA O CURSO PARA VOLTA A PISCAR
+    num.focus()
 }
 function finalizar()
 {
-
+    if(valores.length == 0)
+    {
+        alert("Adicione valores antes de finalizar.")
+    }
+    else
+    {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores)
+        {
+            soma = soma + valores[pos]
+            media = soma / tot
+            if(valores[pos] > maior)
+            {
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor)
+            {
+                menor = valores[pos]
+            }
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O Valor maior é ${maior}</p>`
+        res.innerHTML += `<p>O valor menor é ${menor}</p>`
+        res.innerHTML += `<p>A soma total é ${soma}</p>`
+        res.innerHTML += `<p>A média é ${media}</p>`
+    }
 }
